8d86dcff14bdb152fc17299d4c134802124d8862
Buyun Xu <xubuyun@outlook.com>
2026-03-31 10:25:09 +0800

    fix: register injections and correct local definition capture

--- queries/locals.scm
+++ queries/locals.scm
@@ -2,6 +2,6 @@
 
 (func_arg_list (value_use) @local.definition)
 (block_arg_list (value_use) @local.definition)
-(op_result (value_use) @local.definition)
+(op_result) @local.definition
 
 (value_use) @local.reference
